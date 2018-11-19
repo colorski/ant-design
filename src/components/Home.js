6ef035286a0e9eeefc0d6b9aeb4ba215f9f2c8e3
wangwei <jlsd0302206@163.com>
2018-11-19 22:58:01 +0800

    brief and

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Card, Col, Row } from 'antd';
+import { Layout, Card, Col, Row, Icon } from 'antd';
 import moment from 'moment';
 import Header from '../containers/HeaderContainer';
 import Footer from './Footer';
@@ -9,54 +9,49 @@ export default class Home extends Component {
   render () {
     const { Content } = Layout;
     const lastSevenDays = moment().subtract(7, 'days').format('YYYY.MM.DD') + '~' + moment().format('YYYY.MM.DD');
+    const cardTitle = (txt) => <div className="flex"><p>{txt}</p><p>目标</p><p>完成率</p></div>
     
     return <Layout className="layout ski-layout">
       <Header />
 
       <Content>
         
-        <Card title="简报" bordered={false} className="ski-card" extra={<span>近7天（{ lastSevenDays }）</span>}>
+        <Card title="简报" bordered={false} className="ski-card brief" extra={<span>近7天（{ lastSevenDays }）</span>}>
           <Row gutter={24}>
             <Col span={4}>
-              <Card title="新增客户">12</Card>
+              <Card title="新增客户"><b>12</b><Icon type="right" /></Card>
             </Col>
             <Col span={4}>
-              <Card title="新增联系人">55</Card>
+              <Card title="新增联系人"><b>12</b><Icon type="right" /></Card>
             </Col>
             <Col span={4}>
-              <Card title="新增商机">34</Card>
+              <Card title="新增商机"><b>12</b><Icon type="right" /></Card>
             </Col>
             <Col span={4}>
-              <Card title="访客计划">25</Card>
+              <Card title="访客计划"><b>12</b><Icon type="right" /></Card>
             </Col>
             <Col span={4}>
-              <Card title="沟通日志">25</Card>
+              <Card title="沟通日志"><b>12</b><Icon type="right" /></Card>
             </Col>
             <Col span={4}>
-              <Card title="工作日志">25</Card>
+              <Card title="工作日志"><b>12</b><Icon type="right" /></Card>
             </Col>
           </Row>
         </Card>
 
-        <Card title="指标" bordered={false} className="ski-card">
+        <Card title="指标" bordered={false} className="ski-card ski-pad" extra={<span>（销售指标 / 单位：万元）</span>}>
           <Row gutter={24}>
-            <Col span={4}>
-              <Card title="新增客户">12</Card>
+            <Col span={6}>
+              <Card title={cardTitle('今日')}><div className="flex"><p>12</p><p>12</p><p>100%</p></div></Card>
             </Col>
-            <Col span={4}>
-              <Card title="新增联系人">55</Card>
+            <Col span={6}>
+              <Card title={cardTitle('本周')}><div className="flex"><p>12</p><p>12</p><p>100%</p></div></Card>
             </Col>
-            <Col span={4}>
-              <Card title="新增商机">34</Card>
+            <Col span={6}>
+              <Card title={cardTitle('本月')}><div className="flex"><p>12</p><p>12</p><p>100%</p></div></Card>
             </Col>
-            <Col span={4}>
-              <Card title="访客计划">25</Card>
-            </Col>
-            <Col span={4}>
-              <Card title="新增沟通日志">25</Card>
-            </Col>
-            <Col span={4}>
-              <Card title="工作日志">25</Card>
+            <Col span={6}>
+              <Card title={cardTitle('本年')}><div className="flex"><p>12</p><p>12</p><p>100%</p></div></Card>
             </Col>
           </Row>
         </Card>
